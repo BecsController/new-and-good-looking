98f7fed40e3721edee4ef9de70b9f54a87749be1
Stanley Roache <stan.roache@gmail.com>
2018-06-08 11:53:59 +1200

    tournament-logic.js keeps track of json data in variable and has total read/write functions

--- tournament-logic.js
+++ tournament-logic.js
@@ -1,13 +1,21 @@
 const path = require('path')
-const dataPath = './data.json'
+const fs = require('fs')
+
+const dataPath = './data/test.json'
 
 //need methods for tournament, tier and fight
 //method to get tournament participants
 
-var allData;
-
+var allData = {test: 'test'};
+writeData()
 
 function readDataIn() {
   let raw = fs.readFileSync(dataPath)
   allData = JSON.parse(raw)
+}
+
+function writeData() {
+  let stringified = JSON.stringify(allData, null, 2)
+  fs.writeFileSync(dataPath, stringified)
+  console.log('writing data');
 }
