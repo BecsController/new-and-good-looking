23c4fd3229f7665e1da07df9089bcc08eaded235
Stanley Roache <stan.roache@gmail.com>
2018-06-08 11:46:46 +1200

    required path, wrote function to load data

--- tournament-logic.js
+++ tournament-logic.js
@@ -1,2 +1,13 @@
+const path = require('path')
+const dataPath = './data.json'
+
 //need methods for tournament, tier and fight
 //method to get tournament participants
+
+var allData;
+
+
+function readDataIn() {
+  let raw = fs.readFileSync(dataPath)
+  allData = JSON.parse(raw)
+}
